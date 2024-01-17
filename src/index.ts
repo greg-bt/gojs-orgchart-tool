c80a3d6ffcc45c17ee5cf517d36d4d08b95389b8
Greg <52139156+greg-bt@users.noreply.github.com>
2024-01-17 18:49:55 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { ClickCreatingTool, Diagram, Model, Part, Point, ToolManager, TreeLayout, TreeModel } from "gojs";
 import LinkTemplate from "./templates/link.template";
 import NodeTemplate from "./templates/node.template";
-import SaveChart from "./functions/save.function";
+import SaveJson, { SaveSVG } from "./functions/save.function";
 import LoadModel from "./functions/load.function";
 import Employee from "./employee";
-import OpenComments, { CommentChange, textarea } from "./functions/comments.function";
+import SelectProps, { ChangeProps, colorselector, textarea } from "./functions/properties.function";
+import { addEmployee } from "./panels/button.panel";
 
 export function UpdateModel(model: Model) { diagram.model = model }
 var filename = "";
@@ -20,6 +21,7 @@ const diagram = new Diagram("diagramContainer",
         allowCopy: false,                                           // Allows nodes to be copied
         allowDelete: true,                                          // Allows nodes to be deleted
         maxSelectionCount: 1,                                       // Allows only one node to be selected at a time
+        validCycle: Diagram.CycleDestinationTree,
 
         // Enable double click tool
         "clickCreatingTool.archetypeNodeData": {},
@@ -28,29 +30,16 @@ const diagram = new Diagram("diagramContainer",
         nodeTemplate: NodeTemplate(),   // Define style for nodes
 
         layout: new TreeLayout({ angle: 90, layerSpacing: 35 }),
+        scrollMode: Diagram.InfiniteScroll
     }
 );
 
-diagram.click = () => OpenComments(diagram.selection.first());
+// Toggle properties panel on click
+diagram.click = () => SelectProps(diagram.selection.first());
 
 // Add new nodes on double click
 diagram.toolManager.clickCreatingTool.insertPart = function (loc: Point) {
-    
-    // Add blank node to diagram
-    var node = ClickCreatingTool.prototype.insertPart.call(this, loc);
-    if (node == null) return null;
-
-    // Create new employee and merge with node data
-    node.data = {
-        ...new Employee(),
-        ...node.data
-    }
-
-    // Scroll to and select new node
-    diagram.select(node);
-    OpenComments(node.part);
-    diagram.commandHandler.scrollToPart(node);
-    return node;
+    return addEmployee(diagram)
 }
 
 /*    ~ Document and Window Listeners ~    */
@@ -58,30 +47,23 @@ diagram.toolManager.clickCreatingTool.insertPart = function (loc: Point) {
 // Warn user before closing page
 window.onbeforeunload = () => {return true;}
 
-textarea.addEventListener("input", () => CommentChange(diagram.selection.first()))
+// Listen for changes to props
+textarea.addEventListener("input", () => ChangeProps(diagram.selection.first()))
+colorselector.addEventListener("input", () => ChangeProps(diagram.selection.first()))
 
 // Save model as json on click
-document.getElementById("saveButton")?.addEventListener("click", () => { SaveChart(diagram.model, filename) });
+document.getElementById("json")?.addEventListener("click", () => { SaveJson(diagram.model, filename) });
+
+// Save diagram svg
+document.getElementById("svg")?.addEventListener("click", () => { SaveSVG(diagram.makeSvg(), filename) });
 
 // Load json model from file
-document.getElementById("fileInput")?.addEventListener("change", (e) => { console.log(LoadModel(e, diagram.model)) });
+document.getElementById("fileInput")?.addEventListener("change", (e) => { filename = LoadModel(e, diagram.model) });
 
 // Override Ctrl+S Shortcut
 document.addEventListener('keydown', e => {
     if (e.ctrlKey && e.key === 's') {
         e.preventDefault();     // Override default
-        SaveChart(diagram.model, filename)
+        SaveJson(diagram.model, filename)
     }
 });
-
-
-/*
-
-TODO:
-    Change picture.panel to take pfp BASE64 image instead
-    Create node script to scrape details
-    Remove email prompt 
-
-
-
-*/
