fffd23a868ff83882e57d4c8f7042c2a84629027
Greg <52139156+greg-bt@users.noreply.github.com>
2024-01-17 00:49:40 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-import { ClickCreatingTool, Diagram, Part, Point, ToolManager, TreeLayout, TreeModel } from "gojs";
+import { ClickCreatingTool, Diagram, Model, Part, Point, ToolManager, TreeLayout, TreeModel } from "gojs";
 import LinkTemplate from "./templates/link.template";
 import NodeTemplate from "./templates/node.template";
+import SaveChart from "./functions/save.function";
+import LoadModel from "./functions/load.function";
 
+var filename = "";
+
+// Create GoJS Diagram
 const diagram = new Diagram("diagramContainer",
     {
-        "toolManager.mouseWheelBehavior": ToolManager.WheelZoom,
-        "commandHandler.zoomFactor": 1.3,
-        "undoManager.isEnabled": true,
-        allowCopy: false,
-        allowDelete: true,
-        maxSelectionCount: 1,
+        "toolManager.mouseWheelBehavior": ToolManager.WheelZoom,    // Mousewheel zoom
+        "commandHandler.zoomFactor": 1.3,                           // Zoom speed
+        "undoManager.isEnabled": true,                              // Allows undo/redo binds
+        allowCopy: false,                                           // Allows nodes to be copied
+        allowDelete: true,                                          // Allows nodes to be deleted
+        maxSelectionCount: 1,                                       // Allows only one node to be selected at a time
 
         // Add new nodes on double click
-        "clickCreatingTool.archetypeNodeData": { // allow double-click in background to create a new node
-
-        },
-        "clickCreatingTool.insertPart": function ( loc: Point ) {  // method override must be function, not =>
+        "clickCreatingTool.archetypeNodeData": {},
+        "clickCreatingTool.insertPart": function ( loc: Point ) {
             const node = ClickCreatingTool.prototype.insertPart.call(this, loc);
             if (node == null) return null;
 
@@ -28,22 +31,23 @@ const diagram = new Diagram("diagramContainer",
             return node;
         },
 
-        linkTemplate: LinkTemplate(),
-        nodeTemplate: NodeTemplate(),
+        linkTemplate: LinkTemplate(),   // Define style for links
+        nodeTemplate: NodeTemplate(),   // Define style for nodes
+
         layout: new TreeLayout({ angle: 90, layerSpacing: 35 })
     }
 );
 
+// Warn user before closing page
 window.onbeforeunload = () => {return true;}
 
+// Save model as json on click
+document.getElementById("saveButton")?.addEventListener("click", () => { SaveChart(diagram.model, filename) });
 
-
+// Load json model from file
+document.getElementById("fileInput")?.addEventListener("change", (e) => { console.log(LoadModel(e, diagram.model)) });
+export function UpdateModel(model: Model) { diagram.model = model }
 
 diagram.model = new TreeModel(
-    [ // the nodeDataArray
-        { key: "A", name:"greg-bt", title: "Engineer", tag: "B02", location: "UK" },
-        { key: "B", parent: "A" },
-        { key: "D", parent: "A" },
-        { key: "C", parent: "B" }
-    ]
+    [{"key":1}]
 );
