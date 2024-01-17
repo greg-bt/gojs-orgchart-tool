c480d76e6bcc8714df5af410ffd633fc12e9a3c1
Greg <52139156+greg-bt@users.noreply.github.com>
2024-01-17 14:42:54 +0000

    Created Employee Class

---
+++ src/employee.ts
@@ -0,0 +1,37 @@
+
+export default class Employee {
+
+    key?:       number | string;    // Key of node
+
+    email:      string;
+    name:       string;
+    title:      string;   
+    tag:        string;
+    location:   string;
+    comments:   string;
+
+    parent?:    number | string;    // Key of parent node
+
+
+    constructor() {
+
+        // Prompt user for employee email
+        let email = prompt("Enter Email Address", "");
+        email = (!email ? "" : email);
+
+        this.email = email;
+
+        // Construct name from email address
+        this.name = email
+            .replace(/\@.*/, "")    // Remove domain name
+            .replace(/\./, " ")     // Replace "." with spaces
+            .replace(/(^\w|\s\w|-\w)/g, c => c.toUpperCase());
+
+        this.title = "Title";
+        this.tag = "Tag";
+        this.location = "Location";
+
+        this.comments = ""
+
+    }
+}
